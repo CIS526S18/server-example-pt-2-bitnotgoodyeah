6eda25831394942b1bc607d5eef25390e37c0400
Susan Burke <seburke@ksu.edu>
2018-02-01 12:03:12 -0600

    errors

--- public/server.js
+++ public/server.js
@@ -3,10 +3,12 @@ const PORT = 3000;
 //import http library
 const http = require('http');
 const fs = require('fs');
+
 const cache = {};
 cache['openhouse.html'] = fs.readFileSync('openhouse.html');
 cache['openhouse.css'] = fs.readFileSync('openhouse.css');
 cache['openhouse.js'] = fs.readFileSync('openhouse.js');
+
 function handleRequest(req, res)
 {
     console.log(req.url);
@@ -14,14 +16,40 @@ function handleRequest(req, res)
    {
        case '/':
        case '/openhouse.html':
-    
-            res.end(cache['openhouse.html']);
+            //res.end(cache['openhouse.html']);
+            fs.readFile('openhouse.html', function(err, data){
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end("Server Error");
+                    return;
+                }
+                res.end(data);
+            })
             break;
        case '/openhouse.css':
-            res.end(cache['openhouse.css']);
+            //res.end(cache['openhouse.css']);
+            fs.readFile('openhouse.css', function(err, data){
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end("Server Error");
+                    return;
+                }
+                res.end(data);
+            })
             break;
        case '/openhouse.js':
-            res.end(cache['openhouse.js']);
+            //res.end(cache['openhouse.js']);
+            fs.readFile('openhouse.js', function(err, data){
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end("Server Error");
+                    return;
+                }
+                res.end(data);
+            })
             break;
        default:
             res.statusCode =  404;
